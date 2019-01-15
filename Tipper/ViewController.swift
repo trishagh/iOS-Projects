320387ba3040bfe63c1feccba7762fd5af5344e3
trishaghosh <tghosh@purdue.edu>
2019-01-15 15:07:50 -0500

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Trisha Ghosh  on 1/15/19.
+//  Copyright © 2019 Trisha Ghosh . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
